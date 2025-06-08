f895e0ac18e0b5f2bb0859fa17e2c137c91d12d4
Enzo Muno <enzomacedomuno@gmail.com>
2025-06-08 17:21:07 -0300

    altera: main.py com as duas defs para executar o pipeline de Extract e Load

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from src.load.load_gsmybody_api import load_all_raw_tables
 from src.load.load_fatsecret_api import run_ingest_fatsecret
 
 if __name__ == "__main__":
-    #load_all_raw_tables()
+    load_all_raw_tables()
     run_ingest_fatsecret()
